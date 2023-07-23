77498adc7f3accbe33ca3fd28da059d35751700b
dev <deliveydev@gmail.com>
2023-07-23 23:58:47 +0300

    Very basic job handling

--- server.go
+++ server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
 )
+
+var connections []*websocket.Conn
 
 func main() {
 	app := fiber.New()
@@ -14,20 +17,42 @@ func main() {
 	app.Get("/ws", websocket.New(handleWebSocket))
 
 	// Start the server
-	err := app.Listen(":9999")
-	if err != nil {
-		panic(err)
+	go func() {
+		err := app.Listen(":9999")
+		if err != nil {
+			panic(err)
+		}
+	}()
+	go handleJobs()
+	select {}
+}
+
+func handleJobs() {
+	// Iterate over an array from one to infinity
+	fmt.Println("handling jobs")
+	for i := 1; ; i++ {
+		fmt.Println("Running job", i)
+		time.Sleep(time.Second)
 	}
 }
 
 // WebSocket handler function
 func handleWebSocket(c *websocket.Conn) {
 	fmt.Println("Client connected!")
+	connections = append(connections, c)
 
 	// Handle WebSocket close event
 	defer func() {
 		fmt.Println("Client disconnected.")
 		err := c.Close()
+
+		for i, conn := range connections {
+			if conn == c {
+				connections = append(connections[:i], connections[i+1:]...)
+				break
+			}
+		}
+
 		if err != nil {
 			fmt.Println("Error closing connection:", err)
 		}
